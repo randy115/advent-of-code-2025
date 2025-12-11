fd12c69a7c38eaa8717dc599b84acfaaa628ac66
Randy Bui <randybui@Randys-MacBook-Air-2.local>
2025-12-10 23:09:04 -0600

    part 1 and 2 solutions

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -8,39 +8,43 @@ import (
 	"github.com/randy115/advent-of-code-2025/internal/input"
 )
 
-func calulateRotations(start *int, dir string, num int) (beg, result int) {
+func calulateRotations(start *int, dir string, num int) (beg, result, quotient int) {
 	begg := *start
+	var q int
 	if dir == "L" {
-		*start = *start - num
-		if *start < 0 {
-			*start = *start + 100
+		q = num / 100
+		if *start > 0 && num%100 >= *start {
+			q += 1
 		}
+		*start = ((*start-num)%100 + 100) % 100
 	}
 	if dir == "R" {
-		*start = *start + num
-		if *start > 99 {
-			*start = *start - 100
-		}
+		q = (*start + num) / 100
+		*start = (*start + num) % 100
 	}
+
 	end := *start
-	return begg, end
+	return begg, end, q
 }
 
-func partOne(combination []string) {
+func rotate(combination []string) {
 	start := 50
-	count := 0
+	partOne := 0
+	partTwo := 0
 	for i, combination := range combination {
 		direction := string(combination[0])
 		num, _ := strconv.Atoi(combination[1:])
-		begg, end := calulateRotations(&start, direction, num)
-		if i < 200 {
+		begg, end, q := calulateRotations(&start, direction, num)
+		if i < 20 {
 			fmt.Printf("start %d | direction %s | turn %d | end %d\n", begg, direction, num, end)
+			fmt.Println(q)
 		}
 		if start == 0 {
-			count++
+			partOne++
 		}
+		partTwo += q
 	}
-	fmt.Println(count)
+	fmt.Printf("Part One Solution: %d | Part Two Solution: %d\n", partOne, partTwo)
 }
 
 func main() {
@@ -48,5 +52,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	partOne(file)
+	rotate(file)
 }
