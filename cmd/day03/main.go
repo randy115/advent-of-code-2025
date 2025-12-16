e74198ac344098b3690d2428c0c4415ffa3e8ee8
randy115 <randyb11595@hotmail.com>
2025-12-16 17:52:08 -0600

    day 3 part 2 working progres

--- cmd/day03/main.go
+++ cmd/day03/main.go
@@ -27,21 +27,49 @@ func maxTwoBatteries(banks []string) {
 }
 
 func maxTwelveBatteries(banks []string) {
-	fmt.Println(len(banks[0]))
+	stack := make([]int, 0, 12)
 	for _, jolt := range banks {
 		remove := len(jolt) - 12
-		stack := make([]int, 0, 12)
+		fmt.Printf("CURRENT BATTERY BANK: %s\n", jolt)
 		for i := 0; i < len(jolt); i++ {
+			fmt.Println("-------------------------------------------------")
+			battery, _ := strconv.Atoi(string(jolt[i]))
+			fmt.Printf("remove count %d\n", remove)
+			fmt.Printf("Current Stack: %d\nCurrent Battery: %d\n", stack, battery)
+			fmt.Println("Current length:", len(stack))
 			if len(stack) == 0 {
-				battery, _ := strconv.Atoi(string(jolt[i]))
+				fmt.Printf("Stack is empty appending battery %d\n", battery)
 				stack = append(stack, battery)
+			} else if stack[len(stack)-1] < battery && remove > 0 {
+				remainingString := len(jolt) - i
+				for len(stack) > 0 && stack[len(stack)-1] < battery && remove > 0 && remainingString > 0 {
+					fmt.Printf("Battery %d < Top Stack Battery %d\nPushing %d\n", stack[len(stack)-1], battery, battery)
+					stack = stack[:len(stack)-1]
+					remove -= 1
+					remainingString -= 1
+				}
+				stack = append(stack, battery)
+			} else if stack[len(stack)-1] < battery && remove == 0 && len(stack) < 12 {
+				stack = append(stack, battery)
+			} else if stack[len(stack)-1] >= battery && len(stack) < 12 {
+				fmt.Printf("Battery %d > Top Stack Battery %d\nPushing %d\n", stack[len(stack)-1], battery, battery)
+				stack = append(stack, battery)
+			} else if len(stack) == 12 {
+				fmt.Println("last else statement")
+				stack[len(stack)-1] = battery
 			}
 		}
+		fmt.Println(len(stack))
+		fmt.Println(stack)
+		fmt.Println("len:", len(stack))
+		fmt.Println("cap:", cap(stack))
+		stack = stack[:0]
+		fmt.Println("-------------------------------------------------")
 	}
 }
 
 func main() {
-	file, err := input.ReadFile("day03.txt")
+	file, err := input.ReadFile("day03e.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
