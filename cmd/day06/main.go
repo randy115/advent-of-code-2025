058917fee2a0b2f57cb92480658306c0c4501152
randy115 <randyb11595@hotmail.com>
2025-12-21 17:43:03 -0600

    day 6 part 02 in progress

--- cmd/day06/main.go
+++ cmd/day06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/randy115/advent-of-code-2025/internal/input"
@@ -15,11 +16,40 @@ func computeMathProblems(file []string) {
 	}
 	row := len(problems)
 	col := len(problems[0])
-
+	var total int
 	for c := 0; c < col; c++ {
+		curr := 0
+		currSym := problems[row-1][c]
 		for r := row - 2; r >= 0; r-- {
+			num, _ := strconv.Atoi(problems[r][c])
+			if currSym == "+" {
+				curr += num
+			} else if curr == 0 {
+				curr = 1
+				curr *= num
+			} else {
+				curr *= num
+			}
 			fmt.Printf("matrix[%d][%d] = %s\n", r, c, problems[r][c])
 		}
+		total += curr
+	}
+	fmt.Println(total)
+}
+
+func computeRightMostMathProblems(file []string) {
+	var problems [][]string
+	for _, num := range file {
+		problems = append(problems, strings.Fields(num))
+	}
+	// row := len(problems)
+	col := len(problems[0])
+	// var total int
+	for c := 0; c < col; c++ {
+		// fmt.Printf("matrix[%d][%d] = %s\n", r, c, problems[r][c])
+	}
+	for _, problem := range problems {
+		fmt.Println(problem)
 	}
 }
 
@@ -28,5 +58,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	computeMathProblems(file)
+	computeRightMostMathProblems(file)
 }
