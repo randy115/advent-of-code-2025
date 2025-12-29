c618b91ed343cd0c6a5af5285b959fe0be70476d
randy115 <randyb11595@hotmail.com>
2025-12-29 01:08:53 -0600

    day 8 part 02 solution

--- cmd/day08/main.go
+++ cmd/day08/main.go
@@ -34,12 +34,13 @@ func find(dsu []int, x int) int {
 	return dsu[x]
 }
 
-func union(dsu []int, a, b int) {
+func union(dsu []int, a, b int, merges *int) {
 	roota := find(dsu, a)
 	rootb := find(dsu, b)
 
 	if roota != rootb {
 		dsu[rootb] = roota
+		*merges--
 	}
 }
 
@@ -71,9 +72,9 @@ func connectJunctionBoxes(boxes []string) {
 	for i := range dsu {
 		dsu[i] = i
 	}
-
-	for i := 0; i < 10; i++ {
-		union(dsu, edges[i].a, edges[i].b)
+	merges := 0
+	for i := 0; i < 1000; i++ {
+		union(dsu, edges[i].a, edges[i].b, &merges)
 	}
 	counts := make(map[int]int)
 
@@ -81,15 +82,60 @@ func connectJunctionBoxes(boxes []string) {
 		root := find(dsu, i)
 		counts[root]++
 	}
+	sizes := make([]int, 0, len(counts))
+	for _, size := range counts {
+		sizes = append(sizes, size)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
+	result := sizes[0] * sizes[1] * sizes[2]
+	fmt.Println(result)
+}
+
+func extendJunctionBoxes(boxes []string) float64 {
+	re := regexp.MustCompile(`(\d+),(\d+),(\d+)`)
+	var points []Point
+	var edges []Edge
+
+	for _, box := range boxes {
+		matches := re.FindStringSubmatch(box)
+		x, _ := strconv.ParseFloat(matches[1], 64)
+		y, _ := strconv.ParseFloat(matches[2], 64)
+		z, _ := strconv.ParseFloat(matches[3], 64)
+		points = append(points, Point{x, y, z})
+	}
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			a := points[i]
+			b := points[j]
+			distance := euclideanDistance(a.x, a.y, a.z, b.x, b.y, b.z)
+			edges = append(edges, Edge{i, j, distance})
+		}
+	}
+
+	sort.Slice(edges, func(i, j int) bool {
+		return edges[i].dist < edges[j].dist
+	})
+	dsu := make([]int, len(points))
 	for i := range dsu {
-		fmt.Println(i, find(dsu, i))
+		dsu[i] = i
+	}
+
+	merges := len(points)
+	for {
+		for _, edge := range edges {
+			union(dsu, edge.a, edge.b, &merges)
+			if merges == 1 {
+				return points[edge.a].x * points[edge.b].x
+			}
+		}
 	}
 }
 
 func main() {
-	file, err := input.ReadFile("day08e.txt")
+	file, err := input.ReadFile("day08.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	connectJunctionBoxes(file)
+	res := extendJunctionBoxes(file)
+	fmt.Println(res)
 }
