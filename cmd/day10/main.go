c9880c5ac22b54205ed32249e941c01f01f4be44
rui <randyb11595@hotmail.com>
2026-01-04 13:33:27 -0600

    day10 part 2 working progress

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -80,6 +80,50 @@ func bfs(desiredState string, buttons [][]int) int {
 	return 0
 }
 
+func bfsJoltage(desiredState string, buttons [][]int, joltageLevels []int) int {
+	visited := make(map[string]struct{})
+	initial := make([]int, len(joltageLevels))
+	queue := [][]int{initial}
+	presses := 0
+	// fmt.Printf("DESIRED STATE %s\n", desiredState)
+	for len(queue) > 0 {
+		nextLevel := []string{}
+		for _, pattern := range queue {
+			// fmt.Printf("CURRENT PATTERN %s\n", pattern)
+			// fmt.Printf("START -------------------------------------------\n\n")
+
+			if desiredState == pattern {
+				fmt.Printf("desired light pattern found %s \n", desiredState)
+				return presses
+			}
+
+			if _, ok := visited[pattern]; !ok {
+				// fmt.Printf("light pattern not found: %s adding to map\n", pattern)
+				visited[pattern] = struct{}{}
+				for _, row := range buttons {
+					light := []byte(pattern)
+					// fmt.Printf("current button set %d\n", row)
+					for _, col := range row {
+						if light[col] == '.' {
+							light[col] = '#'
+						} else {
+							light[col] = '.'
+						}
+					}
+					// fmt.Printf("new light pattern %s\n", light)
+					nextLevel = append(nextLevel, string(light))
+				}
+			}
+			// fmt.Printf("END -------------------------------------------\n\n")
+		}
+		// fmt.Printf("current queue %v\n", nextLevel)
+		// fmt.Printf("current map %v\n", visited)
+		queue = nextLevel
+		presses += 1
+	}
+	return 0
+}
+
 func configureLights(schemes []string) {
 	total := 0
 	for _, scheme := range schemes {
